001d81fea480ba20afedfe74e63bb83771ecc992
Fernando Ronmo <faromo0214@gmail.com>
2024-10-25 09:32:33 -0700

    Email verification successfully implemented.

--- src/main/java/com/example/html/demo/controller/LoginController.java
+++ src/main/java/com/example/html/demo/controller/LoginController.java
@@ -1,8 +1,5 @@
 package com.example.html.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
--- src/main/java/com/example/html/demo/controller/VerificationController.java
+++ src/main/java/com/example/html/demo/controller/VerificationController.java
@@ -42,7 +42,7 @@ public class VerificationController {
         userService.saveUserDetails(user);
 
         model.addAttribute("verificationSuccess", true);
-        return "verify";
+        return "redirect:/login?emailSuccessfullyVerified";
     }
     
 
