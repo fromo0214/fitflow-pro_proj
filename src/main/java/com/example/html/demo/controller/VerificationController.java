cda1427f6b4b6cebfd76632e7007283cf2ddf42f
Fernando Ronmo <faromo0214@gmail.com>
2024-10-25 09:46:03 -0700

    Updated DB setttings to update.

--- src/main/java/com/example/html/demo/controller/VerificationController.java
+++ src/main/java/com/example/html/demo/controller/VerificationController.java
@@ -42,7 +42,7 @@ public class VerificationController {
         userService.saveUserDetails(user);
 
         model.addAttribute("verificationSuccess", true);
-        return "redirect:/login?emailSuccessfullyVerified";
+        return "verify";
     }
     
 
